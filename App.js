487b4395ee83745a38f8f48063fc8781f21c75c5
Christa D. Kitcher <christad.kitcher@Christas-MacBook-Pro.local>
2024-04-11 21:05:54 -0400

    module

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import express from 'express';
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentsRoutes from "./Kanbas/assignments/routes.js";
 import cors from "cors";
 
